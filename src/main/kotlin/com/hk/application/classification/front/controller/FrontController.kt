8464a54d6dc8052efb6e0af38d26c61034c6ea6a
hk <--global>
2019-11-02 23:08:16 +0900

    front controller setting

--- src/main/kotlin/com/hk/application/classification/front/controller/FrontController.kt
+++ src/main/kotlin/com/hk/application/classification/front/controller/FrontController.kt
@@ -1,6 +1,23 @@
 package com.hk.application.classification.front.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
-//分類を検索する
+const val FRONT_BASE_REQUEST_MAPPING = "/front"
+const val FRONT_SEARCH_REQUEST_MAPPING = "/search"
+const val FRONT_FIND_BASE_REQUEST_MAPPING = "/find"
 
-//分類の詳細を取得する
+@Controller(FRONT_BASE_REQUEST_MAPPING)
+class FrontController {
+
+    //分類を検索する
+    @GetMapping(FRONT_SEARCH_REQUEST_MAPPING)
+    fun search() {
+    }
+
+
+    //分類の詳細を取得する
+    @GetMapping(FRONT_FIND_BASE_REQUEST_MAPPING)
+    fun find() {
+    }
+}
--- src/main/kotlin/com/hk/application/classification/front/controller/SearchController.kt
+++ src/main/kotlin/com/hk/application/classification/front/controller/SearchController.kt
@@ -1,7 +1,33 @@
 package com.hk.application.classification.front.controller
 
-//見分け方を設定する
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-//特定の情報を取得する
+// 定数定義
+const val SEARCH_BASE_REQUEST_MAPPING = "/front/{classificationId}/detail"
+const val SEARCH_REQUEST_MAPPING = "/search"
+const val SEARCH_WORD_REQUEST_MAPPING = "/search/word"
+const val FIND_REQUEST_MAPPING = "/find/{itemId}"
 
-//文字列で検索する
+@RestController(SEARCH_BASE_REQUEST_MAPPING)
+class SearchController {
+    //見分け方を設定する
+    @GetMapping(SEARCH_REQUEST_MAPPING)
+    fun conditionSearch() {
+
+    }
+
+    //文字列で検索する
+    @GetMapping(SEARCH_WORD_REQUEST_MAPPING)
+    fun wordSearch() {
+
+    }
+
+    //特定の情報を取得する
+    @GetMapping(FIND_REQUEST_MAPPING)
+    fun find() {
+
+    }
+
+}
+
